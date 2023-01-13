7f7b4278699bbf6823b7d2e9dec6d7732d5f1071
Nicolas Lavv <stalker-n90@yandex.ru>
2023-01-13 17:38:20 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://07dcb763-a4a9-44df-8881-ca57ec6f1fed.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
